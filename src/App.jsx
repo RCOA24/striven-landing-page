81279381ef1e1a7a7b178473696c050ce0b2cdbb
Rodney Austria <mrtangkad59@gmail.com>
2026-01-01 17:51:29 +0800

    intent redirection to browser

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Features from './components/Features';
@@ -7,6 +7,16 @@ import DownloadSection from './components/DownloadSection';
 import Footer from './components/Footer';
 
 function App() {
+  useEffect(() => {
+    const ua = navigator.userAgent || navigator.vendor || window.opera;
+    const isInApp = /FBAN|FBAV|Instagram|Line/i.test(ua);
+
+    if (isInApp && /android/i.test(ua)) {
+      const currentUrl = window.location.href.replace(/^https?:\/\//, '');
+      window.location.href = `intent://${currentUrl}#Intent;scheme=https;package=com.android.chrome;end`;
+    }
+  }, []);
+
   return (
     <div className="min-h-screen bg-white dark:bg-dark transition-colors duration-300">
       <Navbar />
